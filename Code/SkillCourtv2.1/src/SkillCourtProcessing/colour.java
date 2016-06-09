20a0d99683b32bf83eda7f1d49ef37d00d86e77b
Leonheart393 <Gajen.gunasegaram@gmail.com>
2016-06-09 11:00:41 -0400

    Add package for a possible porting of processing code directly.

---
+++ Code/SkillCourtv2.1/src/SkillCourtProcessing/colour.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SkillCourtProcessing;
+
+/**
+ *
+ * @author Gajen
+ */
+public class colour {
+    public int r;
+    public int g;
+    public int b;
+
+    public colour(int r, int g, int b)
+    {
+      this.r = r;
+      this.g = g;
+      this.b = b;
+
+    }
+
+    public int getRed()
+    {
+       return r;
+    }
+    public int getGreen()
+    {
+      return g;
+    }
+    public int getBlue()
+    {
+      return b;
+    }
+}
